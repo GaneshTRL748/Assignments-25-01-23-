7ce7767bcc95d0562406f37a1d9a616711004f04
Ganeshkumar <123441945+GaneshTRL748@users.noreply.github.com>
2023-01-27 19:20:21 +0530

    Update Creditcardoperation.java

--- Assignment6TRL748/src/main/java/creditcard/Creditcardoperation.java
+++ Assignment6TRL748/src/main/java/creditcard/Creditcardoperation.java
@@ -3,7 +3,7 @@ import java.util.*;
 import java.util.logging.Logger;
 public class Creditcardoperation implements Cloneable {
 	         Scanner p=new Scanner(System.in);
-	         Logger l= Logger.getLogger("com.api.jar");
+	         Logger logger= Logger.getLogger("com.api.jar");
             String cardname;
             String cardnumber;
             String expirydate;
@@ -17,26 +17,24 @@ public class Creditcardoperation implements Cloneable {
             	this.cardnumber=cardnumber;
             	this.expirydate=expirydate;
             }
-            boolean checkcardnumber()
+            boolean checkcardnumber(String str)
             {
-            	l.info("ENTER YOUR CARD NUMBER");
-            	String str=p.nextLine();
             	if(this.cardnumber.equals(str))
             	{
-            		l.info("Card number verified successfully.......");
+            		logger.info("Card number verified successfully.......");
             		return true;
             	}
             	else {
-            		l.info("OOPs wrong card number");
+            		logger.info("OOPs wrong card number");
             		return false;
             	}
             }
             public static void main(String[] args) throws CloneNotSupportedException
             {
-            	Logger l= Logger.getLogger("com.api.jar");
-            	Creditcardoperation a1=new Creditcardoperation("ganesh","124717000153950","12.11.2027");
-                l.info(a1.checkcardnumber()+"!!!!");
+            	Logger logger= Logger.getLogger("com.api.jar");
+            	Creditcardoperation a1=new Creditcardoperation("ganesh","124717","12.11.2027");
             	Creditcardoperation a2=(Creditcardoperation)a1.clone();
-            	l.info("Cloned Details:\ncardnumber:"+a2.cardnumber+"\ncardname:"+a2.cardname+"\nexpirydate:"+a2.expirydate);
+            	logger.info("Cloned Details:\ncardnumber:"+a2.cardnumber+"\ncardname:"+a2.cardname+"\nexpirydate:"+a2.expirydate);
+            	logger.info("verification status:"+a1.checkcardnumber(a2.cardnumber));
             }
 }
