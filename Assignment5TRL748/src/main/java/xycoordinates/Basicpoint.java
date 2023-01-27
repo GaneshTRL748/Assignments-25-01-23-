8704261aa1b5b562084a319e196c1f381466a7d8
Ganeshkumar <123441945+GaneshTRL748@users.noreply.github.com>
2023-01-27 19:26:08 +0530

    Update Basicpoint.java

--- Assignment5TRL748/src/main/java/xycoordinates/Basicpoint.java
+++ Assignment5TRL748/src/main/java/xycoordinates/Basicpoint.java
@@ -17,11 +17,11 @@ public class Basicpoint implements Cloneable{
     {
   	  if(this.x.equals(tempx)&&this.y.equals(tempy))
   	  {
-  		  logger.log("X and Y Coordinates are Equal");
+  		  logger.info("X and Y Coordinates are Equal");
   		  return true;
   	  }
   	  else {
-  		  logger.log("X and Y Coordinates are NotEqual");
+  		  logger.info("X and Y Coordinates are NotEqual");
   		  return false;
   	  }
     }
@@ -30,9 +30,9 @@ public class Basicpoint implements Cloneable{
     	          Logger logger= Logger.getLogger("com.api.jar");
             	  Basicpoint a1=new Basicpoint("13.7","13.7");
             	  Basicpoint a2=new Basicpoint("13.7","13.7");
-            	  logger.info("Coordinates status:"+a1.checking(a2.x,a2.y));
+            	  logger.log("Coordinates status:"+a1.checking(a2.x,a2.y));
             	  Basicpoint a3=(Basicpoint)a1.clone();
-            	  logger.info("Cloned Coordinates:X-"+a3.x+" Y-"+a3.y);
+            	  logger.log("Cloned Coordinates:X-"+a3.x+" Y-"+a3.y);
             	  
           }
 }
